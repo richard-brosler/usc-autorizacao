d18ce4b1d8c02e27fe46337335d361e109325cc9
richard-brosler <richard.brosler@gmail.com>
2015-05-21 06:30:35 -0300

    Ajustes

--- src/br/usc/segusc/autorizacao/impl/Autorizacao.java
+++ src/br/usc/segusc/autorizacao/impl/Autorizacao.java
@@ -6,18 +6,24 @@ import br.usc.segusc.autorizacao.exception.LoginInvalidoException;
 import br.usc.segusc.autorizacao.services.IAutorizacaoService;
 
 public class Autorizacao implements IAutorizacaoService {
-
+	private Perfil perfil;
+	private Permissao permissao;
+	public Autorizacao() {
+		perfil = new Perfil();
+		permissao = new Permissao();
+		perfil.setPermissoes(permissao);
+		permissao.setDescricao("teste de permissao");
+	}
 	@Override
 	public Perfil obterPerfis(String login) throws LoginInvalidoException {
-		// TODO Auto-generated method stub
-		return null;
+		perfil.setNome(login);
+		return perfil;
 	}
 
 	@Override
 	public Permissao obterPermissoes(String login)
 			throws LoginInvalidoException {
-		// TODO Auto-generated method stub
-		return null;
+		return permissao;
 	}
 
 }
